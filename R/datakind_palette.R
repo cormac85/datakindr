5ec735e7abd532a39becb3e96c9b643a9930d05f
cormac85 <cormac85@gmail.com>
2017-06-29 23:20:42 +0100

    Adding datakind palette

---
+++ R/datakind_palette.R
@@ -0,0 +1,15 @@
+#' DataKind Recommended Palette
+#'
+#' A list containing a the recommended palette for use when making visualisations
+#' for DataKind.
+#'
+#' @format A list of strings, each is a colour hex code.
+#' \describe{
+#'   \item{datakind_palette}{list of datakind colour hex codes}
+#' }
+"datakind_palette"
+#                  (V Dark Grey,  orange,   brown,    yellow,   white,     cream-ish)
+# datakind_palette <- c("#404040", "#FF6600", "#804D00", "#FF9900", "#FFFFFF", "#EEEEDD")
+#
+# devtools::use_data(datakind_palette)
+
