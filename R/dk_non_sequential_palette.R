e46f0b6e7f31e9a994b602fc0fca8b3634834415
cormac85 <cormac85@gmail.com>
2017-07-04 23:14:57 +0100

    Adding alternative palettes.

---
+++ R/dk_non_sequential_palette.R
@@ -0,0 +1,14 @@
+#' DataKind Recommended Palette For Non-Sequential Data
+#'
+#' A list containing an extended palette for use when making visualisations
+#' for DataKind with data in non-sequential order.
+#'
+#' @format A list of strings, each is a colour hex code.
+#' \describe{
+#'   \item{dk_palette}{List of datakind colour hex codes.}
+#'   \item{colours:}{("#404040", "#FF6600", "#FF9900", "#FFCC33" , "#FFCC99")}
+#' }
+"dk_non_sequential_palette"
+
+# dk_non_sequential_palette <- c("#404040" , "#FF6600", "#FF9900",
+#                                "#FFCC33" , "#FFCC99")
---
+++ R/dk_sequential_palette.R
@@ -0,0 +1,14 @@
+#' DataKind Recommended Palette For Sequential Data
+#'
+#' A list containing an extended palette for use when making visualisations
+#' for DataKind with data in sequential order.
+#'
+#' @format A list of strings, each is a colour hex code.
+#' \describe{
+#'   \item{dk_palette}{List of datakind colour hex codes.}
+#'   \item{colours:}{("#404040", "#804d00" , "#BB6600" , "#FF6600", "#FF9900", "#FFCC33" , "#FFCC99")}
+#' }
+"dk_sequential_palette"
+
+# dk_sequential_palette <- c("#404040" , "#804d00" , "#BB6600" , "#FF6600",
+#                             "#FF9900" , "#FFCC33" , "#FFCC99")
