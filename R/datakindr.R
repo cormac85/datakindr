a2b9c89a5b49e6bf07958daa64ccdb6fba2ad45d
cormac85 <cormac85@gmail.com>
2017-07-08 19:56:27 +0100

    First pass of package documentation. Also playing with automated versioning.

---
+++ R/datakindr.R
@@ -0,0 +1,23 @@
+#' datakindr: Tools And Utilities for DataKind
+#'
+#' Provides tools, utilities, data and more for use by the DataKind
+#' Dublin team. Including a convenient interface to CSO.ie's Statbank and a
+#' ggplot2 theme and colour palette for convenient & consistent visualisation
+#' styles.
+#'
+#' @section Datakindr statbank functions:
+#' For interacting with CSO.ie datasets, the following two functions can be used:
+#' \itemize{
+#' \item{\code{search_statbank_datasets()} returns a data frame with dataset
+#' codes and a description of the dataset to which each code refers.}
+#' \item{Once you've found the dataset you need (either from the Statbank website, or
+#' by using \code{search_statbank_datasets()}) you can use
+#' \code{get_cso_dataset()} to retrieve the dataset as a data.frame.}
+#' }
+#' @section Datakindr Plotting:
+#' Blah blah blah
+#'
+#'
+#' @docType package
+#' @name datakindr
+NULL
