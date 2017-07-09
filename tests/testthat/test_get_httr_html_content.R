18053c7c7823e0a03d78af8bcc7b59a2076bd77b
cormac85 <cormac85@gmail.com>
2017-07-09 21:45:25 +0100

    removing RPushbullet, won't work from Travis. Tidied up a test. Tidied up .travis.yml

--- tests/testthat/test_get_httr_html_content.R
+++ tests/testthat/test_get_httr_html_content.R
@@ -4,6 +4,6 @@ test_that("httr request is successfully parsed into xml document.", {
   mockery::stub(get_httr_html_content, # Where
                 "httr::GET",  # What
                 readRDS("./mocked_data/httr_response_garda.rds")) # How
-  expect_is(get_httr_html_content(create_cso_search_url("garda", 0)),
-            "xml_document")
+  test_results <- get_httr_html_content(create_cso_search_url("garda", 0))
+  expect_is(test_results, "xml_document")
 })
