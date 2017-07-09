3ef41ac0eef4fd75b449b576c097c530c4c69cfa
cormac85 <cormac85@gmail.com>
2017-07-09 16:06:05 +0100

    Added new test when getting statbank dataset. Added dplyr to suggests

--- tests/testthat/test_get_cso_dataset.R
+++ tests/testthat/test_get_cso_dataset.R
@@ -4,7 +4,17 @@ test_that("dataframe is correctly returned from Statbank", {
   mockery::stub(get_cso_dataset, # Where
                 "rjstat::fromJSONstat",  # What
                 readRDS("./mocked_data/sample_statbank_dataset.rds")) # How
-  expect_is(get_cso_dataset("ASR11"), "data.frame")
-  expect_is(get_cso_dataset("ASR11")[[1]], "character")
-  expect_length(get_cso_dataset("ASR11"), 5)
+  test_results <- get_cso_dataset("ASR11")
+  expected_results <-
+    data.frame(Sex = "Both sexes",
+               `Intermediate Occupational Group` = "All occupations",
+               `Census Year` = "1991",
+               Statistic = "1996 Population Aged 15 Years and Over at Work (Number)",
+               value = 1149080,
+               stringsAsFactors = FALSE, check.names = FALSE)
+
+  expect_is(test_results, "data.frame")
+  expect_is(test_results[[1]], "character")
+  expect_length(test_results, 5)
+  expect_equal(test_results[1,], expected_results)
 })
